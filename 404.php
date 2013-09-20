d586f3fe6b7b423b987113f0fc956831faf0fa8f
stevejpurves <stevejpurves@gmail.com>
2013-09-21 00:04:08 +0100

    initial commit

---
+++ 404.php
@@ -0,0 +1,7 @@
+<div id="post-0" class="post error404 not-found">
+	<h1 class="entry-title"><?php _e( 'Not Found', 'hbd-theme' ); ?></h1>
+	<div class="entry-content">
+		<p><?php _e( 'Sorry! That file does not exist. 404, etc...', 'hbd-theme' ); ?></p>
+		<?php get_search_form(); ?>
+	</div><!-- .entry-content -->
+</div><!-- #post-0 -->
